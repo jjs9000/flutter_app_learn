b83ef93da13df211ebaed73689e2118a740677bc
jjs9000 <farriszamrie15@gmail.com>
2021-05-17 20:00:55 +0800

    added widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('HELLO WORLD'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/ning.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
